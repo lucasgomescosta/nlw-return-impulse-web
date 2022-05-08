7dbb6f42be61280f21f306eea09c2370cce9286b
Lucas Gomes <lucas.costa@sefaz.rr.gov.br>
2022-05-08 19:39:10 -0400

    first

--- src/components/WidgetForm/ScreenshortButton.tsx
+++ src/components/WidgetForm/ScreenshortButton.tsx
@@ -15,28 +15,33 @@ export function ScreenshortButton({
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false);
 
   async function handleTakeScreenshot() {
+    console.log(isTakingScreenshot)
     setIsTakingScreenshot(true);
 
     const canvas = await html2canvas(document.querySelector('html')!);
     const base64image = canvas.toDataURL('image/png');
 
     onScreenshotTook(base64image);
+    console.log(screenshot)
     setIsTakingScreenshot(false);
   }
 
+ 
   if(screenshot) {
-    <button
-      type="button"
-      className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
-      style={{ 
-        backgroundImage: `url(${screenshot})`,
-        backgroundPosition: 'right bottom',
-        backgroundSize: 180,
-      }}
-      onClick={() => onScreenshotTook(null)}
-    >
-      <Trash weight="fill" />
-    </button>
+    return (
+      <button
+        type="button"
+        className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+        style={{ 
+          backgroundImage: `url(${screenshot})`,
+          backgroundPosition: 'right bottom',
+          backgroundSize: 180,
+        }}
+        onClick={() => onScreenshotTook(null)}
+      >
+        <Trash weight="fill" />
+      </button>
+    )
   }
 
   return (
