877d9d099836214502e3cf57c258c735beb68ef4
Lucas Gomes <lucas.costa@sefaz.rr.gov.br>
2022-05-08 16:42:44 -0400

    first commit

--- src/components/WidgetForm/ScreenshortButton.tsx
+++ src/components/WidgetForm/ScreenshortButton.tsx
@@ -27,12 +27,12 @@ export function ScreenshortButton({
   if(screenshot) {
     <button
       type="button"
-      className="p-1 w-10 h-10 rounded-md border-transparent justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+      className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
       onClick={() => onScreenshotTook(null)}
       style={{ 
         backgroundImage: `url(${screenshot})`,
         backgroundPosition: 'right bottom',
-        backgroundSize: 100,
+        backgroundSize: 180,
       }}
     >
       <Trash weight="fill" />
@@ -42,7 +42,8 @@ export function ScreenshortButton({
   return (
     <button
     type="button"
-    className="p-2 bg-zinc-800 roudend-md border-transparent hover:bg-zinc-700 transition-colors"
+    className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+    onClick={handleTakeScreenshot}
     >
      { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" />} 
     </button>
